15bbff9cd997f23deb0262dfaee62b760d8edea7
kohyerim <kohyerim78@gmail.com>
2019-12-19 15:07:10 +0900

    readme add

--- Prediction.py
+++ Prediction.py
@@ -3,14 +3,18 @@ import pandas as pd
 
 util = PredictionUtil()
 util.read('dataset.csv')
+print(util.df.columns)
 util.drop(['Unnamed: 0', 'brand_name', 'Location', 'New_Price'])
+util.boxplot('Owner_Type', 'Price')
+util.lmplot('Power_upd', 'Price', 'Owner_Type')
+util.df = pd.get_dummies(util.df)
+print(util.df.columns)
+util.heatmap(['Kilometers_Driven', 'Seats', 'Price', 'Mileage_upd', 'Engine_upd',
+               'Power_upd', 'Year_upd', 'Fuel_Type_CNG', 'Fuel_Type_Diesel',
+               'Fuel_Type_Electric', 'Fuel_Type_LPG', 'Fuel_Type_Petrol',
+               'Transmission_Automatic', 'Transmission_Manual', 'Owner_Type_First',
+               'Owner_Type_Fourth & Above', 'Owner_Type_Second', 'Owner_Type_Third'])
 
-util.df = pd.get_dummies(util.df, drop_first=True)
-# util.boxplot('Owner_Type', 'Price')
-# util.heatmap(['Kilometers_Driven', 'Seats', 'Price', 'Mileage_upd', 'Engine_upd',
-#                'Power_upd', 'Year_upd', 'Fuel_Type_Diesel', 'Fuel_Type_Electric',
-#                'Fuel_Type_LPG', 'Fuel_Type_Petrol', 'Transmission_Manual',
-#                'Owner_Type_Fourth & Above', 'Owner_Type_Second', 'Owner_Type_Third'])
 util.run_all(['Power_upd', 'Engine_upd', 'Transmission_Manual'], 'Price')
 print('\n# add Year')
 util.run_all(['Power_upd', 'Engine_upd', 'Transmission_Manual', 'Year_upd'], 'Price')
